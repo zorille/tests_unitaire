04ee8f624e63a88578c341fbcd18192f2db9a22f
Damien Vargas <zorille@free.FR>
2018-12-04 14:45:05 +0000

    Ajout de nouveaux test : Dolibarr, librenms et opnsense

---
+++ php_framework/opnsense/opnsense_datasTest.php
@@ -0,0 +1,113 @@
+<?php
+namespace Zorille\opnsense;
+use Zorille\framework as Core;
+use \Exception as Exception;
+
+/**
+ * @author dvargas
+ * @package Lib
+ *
+ */
+if (! defined ( '__DOCUMENT_ROOT__' )) {
+	require_once $_SERVER ["PWD"] . '/prepare.php';
+}
+
+/**
+ */
+class datasTest extends Core\MockedListeOptions {
+	/**
+     * @var datas
+     */
+	protected $object;
+
+	/**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+	protected function setUp() {
+		ob_start ();
+		
+		$utilisateurs = $this ->createMock('Zorille\framework\utilisateurs' );
+		$utilisateurs ->expects ( $this ->any () ) 
+			->method ( 'retrouve_utilisateurs_array' ) 
+			->will ( $this ->returnValue ( $utilisateurs ) );
+		$utilisateurs ->expects ( $this ->any () ) 
+			->method ( 'getUsername' ) 
+			->will ( $this ->returnValue ( 'USER1' ) );
+		$utilisateurs ->expects ( $this ->any () ) 
+			->method ( 'getPassword' ) 
+			->will ( $this ->returnValue ( 'PASS1' ) );
+		
+		$this->object = new datas ( false, "TESTS datas" );
+		$this->object ->setListeOptions ( $this ->getListeOption () ) 
+			->setObjetUtilisateurs ( $utilisateurs );
+	}
+
+	/**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+	protected function tearDown() {
+		ob_end_clean ();
+	}
+
+	/**
+	 * @covers Zorille\opnsense\datas::retrouve_param
+	 */
+	public function testRetrouve_param_exception1() {
+		$this->object ->getListeOptions () 
+			->method ( 'verifie_variable_standard' ) 
+			->will ( $this ->returnValue ( false ) );
+		$this->object ->getListeOptions () 
+			->expects ( $this ->any () ) 
+			->method ( 'renvoi_variables_standard' ) 
+			->will ( $this ->returnValue ( array ( 
+				"#comment" => "et voila un commentaire", 
+				"nom" => "VMW_TEST" ) ) );
+		
+		$this ->expectException(Exception::class);
+        $this->expectExceptionMessage( '(TESTS datas) Il manque le parametre : opnsense_machines_serveur' );
+		$this->object ->retrouve_param ();
+	}
+
+	/**
+	 * @covers Zorille\opnsense\datas::retrouve_param
+	 */
+	public function testRetrouve_param_valide() {
+		$this->object ->getListeOptions () 
+			->method ( 'verifie_variable_standard' ) 
+			->will ( $this ->onConsecutiveCalls ( true, true ) );
+		$this->object ->getListeOptions () 
+			->expects ( $this ->any () ) 
+			->method ( 'renvoi_variables_standard' ) 
+			->will ( $this ->onConsecutiveCalls ( array ( 
+				"#comment" => "et voila un commentaire", 
+				"nom" => "VMW_TEST" ), array ( 
+				"wsdl" => "WSDL_NAME" ) ) );
+		$this ->assertSame ( $this->object, $this->object ->retrouve_param () );
+		$this ->assertEquals ( array ( 
+				'nom' => 'VMW_TEST' ), $this->object ->getServeurDatas () );
+	}
+
+	/**
+	 * @covers Zorille\opnsense\datas::valide_presence_data
+	 */
+	public function testvalide_presence_data() {
+		$this->object ->setServeurData ( array ( 
+				"TEST" => array ( 
+						"nom" => "NOMMACHINE" ), 
+				"TEST2" => array ( 
+						"nom" => "NOMMACHINE2" ) ) );
+		$this ->assertEquals ( array ( 
+				"nom" => "NOMMACHINE", 
+				'username' => 'USER1', 
+				'password' => 'PASS1' ), $this->object ->valide_presence_data ( "NOMMACHINE" ) );
+		$this ->assertEquals ( array ( 
+				"nom" => "NOMMACHINE2", 
+				'username' => 'USER1', 
+				'password' => 'PASS1' ), $this->object ->valide_presence_data ( "NOMMACHINE2" ) );
+		$this ->assertFalse ( $this->object ->valide_presence_data ( "NOMMACHINE3" ) );
+	}
+
+}
+?>
